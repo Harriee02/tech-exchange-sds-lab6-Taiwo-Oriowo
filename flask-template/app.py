f0ae881b8d861e28a01977dc8fd916b8e3b12c5d
Taiwo <harrie@techexchange.in>
2022-03-22 04:38:38 +0000

    Lab 6 push

--- flask-template/app.py
+++ flask-template/app.py
@@ -1,22 +1,7 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# ---- YOUR APP STARTS HERE ----
-# -- Import section --
 from flask import Flask
-# from flask import render_template
-# from flask import request
+from flask import render_template
+from flask import request
+from model import checkAnswer
 
 
 # -- Initialization section --
@@ -27,4 +12,30 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world"
+    return render_template("index.html")
+
+@app.route('/results',methods = ["GET", "POST"])
+def results():
+    point = 0
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        answer = {"New York": request.form["New York"], "California": request.form["California"],"Alabama": request.form["Alabama"] , "Ohio": request.form["Ohio"] , "Utah": request.form["Utah"]}
+        results = checkAnswer(answer)
+        newYork = results["New York"]
+        california = results["California"]
+        alabama = results["Alabama"]
+        ohio = results["Ohio"]
+        utah = results["Utah"]
+        if newYork:
+            point += 1
+        if california:
+            point += 1
+        if alabama:
+            point += 1
+        if ohio:
+            point += 1
+        if utah:
+            point += 1
+        return render_template('scorepage.html', point= point, newYork=newYork, california = california, alabama=alabama, ohio=ohio, utah=utah)
+        
--- flask-template/model.py
+++ flask-template/model.py
@@ -1,14 +1,19 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+answerDict = {
+"New York" : "albany",
+"California" : "sacramento",
+"Alabama" : "montgomery",
+"Ohio": "columbus",
+"Utah": "salt lake city"
+}
 
+def checkAnswer(answer):
+    resultsDict = {}
+    for k,v in answer.items():
+        if answerDict[k] == v:
+            resultsDict[k] = True
+        else:
+            resultsDict[k] = False
+    return resultsDict
+
+
+#print(checkAnswer({"New York": "albany","Utah": "salt lake city"}))
